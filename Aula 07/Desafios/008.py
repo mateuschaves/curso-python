edc6bc410e61e09c83aa270580a63d11d3082c6c
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-07-22 13:10:26 -0300

    Desafio 008

---
+++ Aula 07/Desafios/008.py
@@ -0,0 +1,7 @@
+# Escreva um programa que leia um valor em metros
+# e o exiba convertido em centímetros e milímetros.
+
+metros = int(input('Informe a distância em metros: '))
+
+print('{} metros é igual a: {} centímetros !'.format(metros, metros*100))
+print('{} metros é igual a: {} milímetros !'.format(metros, metros*1000))
