cd776a0b8d240e258ffa2d1dbe31be8d9e8a9964
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-07-22 13:03:05 -0300

    Desafio 006

---
+++ Aula 07/Desafios/006.py
@@ -0,0 +1,7 @@
+# Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+
+n = float(input('Digite um número: '))
+
+print('O dobro de {} é {}'.format(n, n*2))
+print('O tripo de {} é {}'.format(n, n*3))
+print('A raiz quadrada de {} é {}'.format(n, n**(1/2)))
