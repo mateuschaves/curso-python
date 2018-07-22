210b255e13168f3649c6f2333e6cb558d80645d0
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-07-22 12:58:08 -0300

    Desafio 005

---
+++ Aula 07/Desafios/005.py
@@ -0,0 +1,5 @@
+# Faça um programa que leia um número inteiro e mostre na tea o seu sucessor e seu antecessor.
+
+n = int(input('Digite um número inteiro: '))
+print('O antecessor de {} é {}'.format(n, n - 1))
+print('O sucessor de {} é {}'.format(n, n + 1))
