a516a79cead271144f742f0503dd4b314226c1d8
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-07-22 12:47:44 -0300

    calculadora

---
+++ Aula 07/calculadora.py
@@ -0,0 +1,16 @@
+n1 = int(input('Um valor: '))
+n2 = int(input('Outro valor: '))
+
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+mo = n1 % n2
+
+print('Soma: {}'.format(s))
+print('Multiplicação: {}'.format(m))
+print('Divisão: {}'.format(d))
+print('Divisão inteira: {}'.format(di))
+print('Potência: {}'.format(e))
+print('Resto da divisão: {}'.format(mo))
