7aca12996e0dfaf5d7619aa5d3515106a85413c2
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-07-22 13:06:24 -0300

    Desafio 007

---
+++ Aula 07/Desafios/007.py
@@ -0,0 +1,7 @@
+# Desenvolva um programa que leia as duas notas de um aluno,
+# calcule e mostre a sua méia.
+
+nota1 = float(input('Informe a primeira nota: '))
+nota2 = float(input('Informe a segunda nota: '))
+
+print('A média do aluno é igual a: {:.2f}'.format( (nota1 + nota2)/2 ))
