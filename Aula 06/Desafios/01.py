85f9fc7f9aa073fa622e2c310dc28c44bf378d9d
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-07-22 02:39:11 -0300

    Desafio 01

---
+++ Aula 06/Desafios/01.py
@@ -0,0 +1,4 @@
+# Ler dois números e mostra a soma.
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+print('A soma entre {} e {} é {},'.format(n1, n2, n1 + n2))
