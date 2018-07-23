74ec3f190ce1d4814fb87a39bd820ca5ad04705f
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-07-23 01:02:19 -0300

    correção

--- Aula 08/Desafios/021.py
+++ Aula 08/Desafios/021.py
@@ -1,8 +1,8 @@
 # Faça um programa em python que abra
 # e reproduza um arquivo mp3.
 
-import mp3play
-
-filename = r'C:\Users\mateus\Downloads\musicao.mp3'
-clip = mp3play.load(filename)
-clip.play()
+import pygame
+pygame.init()
+pygame.mixer.music.load('musicao.mp3')
+pygame.mixer.music.play()
+pygame.event.wait()
