e089ccb540bca8be58e411ccfc399582cbfb9062
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-07-22 22:18:59 -0300

    Desafio 014

---
+++ Aula 07/Desafios/014.py
@@ -0,0 +1,7 @@
+# Conversor de temperaturas.
+
+celsius = float(input('Informe a temperatura em celsius: '))
+
+fahrenheit = (celsius * 1.8) + 32
+
+print('A temperatura em fahrenheit é {:.2f} !'.format(fahrenheit))
