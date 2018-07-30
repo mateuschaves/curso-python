f720c3ffd54768a20debf90872122e383cdd005d
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-07-30 04:17:39 -0300

    Desafio 024

---
+++ Aula 09/Desafios/024.py
@@ -0,0 +1,8 @@
+# Crie um programa que leia o nome
+# de uma cidade e diga se ela começa
+# ou não com o nome "SANTO".
+
+frase = input("Digite uma frase: ").split()
+one = frase[0]
+print('SANTO' in one)
+
