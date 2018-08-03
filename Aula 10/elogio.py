64272d59bcf6e860e00521060a133ddd35c70061
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-08-03 04:13:57 -0300

    Aula 10

---
+++ Aula 10/elogio.py
@@ -0,0 +1,6 @@
+nome = input('Qual é o seu nome ? ').strip()
+if nome.upper() == 'MATEUS':
+    print('Que nome massa bixo !')
+else:
+    print('Teu nome é tão bosta igual a vida de Mateus .-.')
+print('Oi {} .-.'.format(nome))
---
+++ Aula 10/media.py
@@ -0,0 +1,6 @@
+n1 = float(input('Digite sua primeira nota: '))
+n2 = float(input('Digite sua segunda nota: '))
+
+media = (n1 + n2) / 2
+
+print('Aprovado !' if media >= 7 else 'Reprovado !')
