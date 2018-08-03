69d64ae4c18c50696f48a37bf22e3f3443db33bd
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-08-03 03:24:35 -0300

    Desafio 023

--- Aula 09/Desafios/023.py
+++ Aula 09/Desafios/023.py
@@ -3,4 +3,14 @@
 # um dos dígitos separados
 
 
-# NÃO SEI AKSDJKASJD
+num = int(input('Informe um número: '))
+
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+
+print('Unidade: {}'.format(u))
+print('Dezena: {}'.format(d))
+print('Centena: {}'.format(c))
+print('Milhar: {}'.format(m))
