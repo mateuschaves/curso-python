ebc0b56880badcfb315ccdf7cb66fbb4f2300193
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-08-03 04:17:09 -0300

    musiquinha

--- Aula 10/Desafios/28.py
+++ Aula 10/Desafios/28.py
@@ -3,6 +3,16 @@
 # para o usuário tentar descobrir qual foi o
 # número escolhido pelo computador.
 
+
+"""
+    And so Sally can wait
+    She knows its too late as we're walking on by
+    Her soul slides away
+    "But don't look back in anger" I heard you say
+
+    Don't Look Back In Anger - Oasis ♪♫
+"""
+
 import random
 
 s = random.randint(0, 5)
