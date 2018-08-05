2831f1d677f02dff067ee8d2726469a97e30a9cf
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-08-05 03:47:12 -0300

    Aula 12

---
+++ Aula 12/for.py
@@ -0,0 +1,9 @@
+for c in range(0, 6):
+    print(c)
+print('FIM')
+
+print('-='*10)
+
+for c in range(6, 0, -1):
+    print(c)
+
---
+++ Aula 12/somatorio.py
@@ -0,0 +1,6 @@
+s = 0
+
+for c in range(0, 5):
+    n = int(input('Digite um valor: '))
+    s += n
+print('O somatório é de: {}'.format(s))
