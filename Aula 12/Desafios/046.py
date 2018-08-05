0bc439193352ddd84cc88d1d1b0233b61ff9d437
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-08-05 03:46:19 -0300

    Aula 12

---
+++ Aula 12/Desafios/046.py
@@ -0,0 +1,21 @@
+"""
+    Faça um programa que mostre na tela
+    uma contagem regressiva para o es-
+    touro de fogos de artifício, indo
+    de 10 até 0, com uma pausa de 1
+    segundo entre eles.
+
+    I went too far when I was begging on my knees
+    Begging for your arms, for you to hold them around me
+    I went too far and kissed the ground beneath your feet
+
+    Waiting for your love, waiting for our eyes to meet
+
+    I Went Too Far - Aurora
+"""
+
+from time import sleep
+
+for c in range(10, -1, -1):
+    print(c)
+    sleep(1)
