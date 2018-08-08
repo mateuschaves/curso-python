0c2753ad633fbdd4e8711efe66e82cb50af1948d
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-08-08 02:55:46 -0300

    Desafio 067

---
+++ Aula 14/Desafios/067.py
@@ -0,0 +1,23 @@
+"""
+    Faça um programa que mostre a tabuada de vários números,
+    um de cada vez, para cada valor digitado pelo usuário. O
+    programa será interrompido quando o número solicitado for
+    negativo.
+
+    Fica
+    Fica, me queira e queira ficar
+    Fica
+    Faz o que quiser de mim
+    Contanto que não falte tempo pra me amar
+
+    Fica - Anavitório ♪♫
+"""
+
+while True:
+    n = int(input('Informe o número da tabuada: '))
+    if n == 0:
+        break
+    for c in range(0, 11):
+        print(f'{n} x {c} = {c*n}')
+
+
