4f5973c58a2b10d2adffdcfb4ca6f7e9f7b537bf
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-08-09 04:23:21 -0300

    Musiquinhaa

--- Aula 15/Desafios/073.py
+++ Aula 15/Desafios/073.py
@@ -10,6 +10,17 @@
     C) Uma lista com os times em ordem alfabética.
 
     D) Em que posição na tabela está o time da Chapecoense.
+
+    Havia uma sacola com uma mascara
+    E uma garota a encontrou
+    De início teve medo
+    Não sabia o que fazer
+    E nem como compor
+    Mas colocou sobre o seu rosto
+    E lhe caiu tão bem
+    Ficou
+
+    Mascarados - Rubel ♪♫
 """
 
 tabela = ('São Paulo', 'Flamengo', 'Internacional', 'Grêmio', 'Atlético', 'Palmeiras', 'Corinthians', 'Cruzeiro', 'Fluminense', 'América-MG', 'Botafogo', 'Sport Recife', 'Vasco da Gama', 'EC Vitória', 'Santos', 'Bahia', 'Chapecoense', 'Ceará SC', 'Atlético PR', 'Paraná')
