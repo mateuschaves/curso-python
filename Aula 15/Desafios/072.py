02a1c76edbbee640e81355f2b5fc999a5c416e64
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-08-09 03:53:06 -0300

    Mas só chove choveeeeeeeeeee ♪♫

--- Aula 15/Desafios/072.py
+++ Aula 15/Desafios/072.py
@@ -3,6 +3,17 @@
     preenchida com uma contagem por extenso, de zero
     até vinte. Seu programa deverá ler um número pelo
     teclado (entre 0 e 20) e mostrá-lo por extenso.
+
+    Se um dia eu pudesse ver
+    Meu passado inteiro
+    E fizesse parar de chover
+    Nos primeiros erros
+    Meu corpo viraria sol
+    Minha mente viraria ar
+    Mas só chove, chove
+    Chove, chove
+
+    Primeiros Erros - Capital Inicial ♪♫
 """
 
 extenso = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
