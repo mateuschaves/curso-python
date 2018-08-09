86d156684611ced46293d618d87b65415803d08f
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-08-09 03:51:13 -0300

    Aula 15

---
+++ Aula 15/tuplas.py
@@ -0,0 +1,12 @@
+lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim')
+print(lanche[1])
+print(lanche[-1])
+print(lanche[0:])
+
+
+
+lanche = ('miojo', 'refrigerante', 'pastel', 'bolo')
+
+for pos, comida in enumerate(lanche):
+    print(comida, pos)
+
