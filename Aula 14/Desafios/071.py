2f9bcb70e2391d6b1809a810ce2f6a218db0f66c
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-08-08 22:29:03 -0300

    Desafio 071

--- Aula 14/Desafios/071.py
+++ Aula 14/Desafios/071.py
@@ -17,49 +17,26 @@
 """
 
 valor = int(input('Informe o valor a ser sacado: '))
-
-um = 0
-dez = 0
-vinte = 0
-cinquenta = 0
+total = valor
+cedula = 50
+total_cedulas = [0, 0, 0, 0]
 while True:
-    if valor // 1 % 10 != 0:
-        um = valor // 1 % 10
-    dezena = valor // 10 % 10
-    centena = valor // 100 % 10
-    milhar = valor // 1000 % 10
-    dezena_milhar = valor // 10000 % 10
-    centena_milhar = valor // 100000 % 10
-    if dezena == 1:
-        dez += 1
-    if dezena > 1 and dezena < 5:
-        if dezena == 4:
-            vinte += 2
-        elif dezena == 3:
-            vinte += 1
-            dez += 1
-        elif dezena == 2:
-            vinte += 1
-    if dezena == 5:
-        cinquenta += 1
-    if dezena < 100:
-        if dezena % 2 == 0:
-            vinte += dezena // 2
-        else:
-            cinquenta += 1
-            if (dezena - 5) % 2 == 0:
-                vinte += (dezena - 5) // 2
-    if centena != 0:
-        cinquenta += centena * 2
-    if milhar != 0:
-        cinquenta += (milhar*1000) // 50
-    if dezena_milhar != 0:
-        cinquenta += (dezena_milhar*10000) // 50
-    if centena_milhar != 0:
-        cinquenta += (centena_milhar*100000) // 50
-    break
-print(f'Notas de R$ 1: {um}')
-print(f'Notas de R$ 10: {dez}')
-print(f'Notas de R$ 20: {vinte}')
-print(f'Notas de R$ 50: {cinquenta}')
+    if total == 0:
+        break
+    elif total >= 50:
+        total -= 50
+        total_cedulas[0] += 1
+    elif total >= 20:
+        total -= 20
+        total_cedulas[1] += 1
+    elif total >= 10:
+        total -= 10
+        total_cedulas[2] += 1
+    else:
+        total -= 1
+        total_cedulas[3] += 1
 
+print(f'Notas de R$ 50: {total_cedulas[0]}')
+print(f'Notas de R$ 20: {total_cedulas[1]}')
+print(f'Notas de R$ 10: {total_cedulas[2]}')
+print(f'Notas de R$ 1: {total_cedulas[3]}')
